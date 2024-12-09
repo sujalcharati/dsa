fa788f99a4b15e65c7bcd5a9cd0de351fbb0e024
sujalcharati <sujalc024@gmail.com>
2024-12-09 08:58:37 +0530

    pattern 7

---
+++ pattern/p7.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number of rows";
+    cin >> n;
+    int rows = 1;
+    while (rows <= n)
+    {
+        int col = 1;
+        int val = rows;
+        while (col <= rows)
+        {
+            cout << val << " ";
+            col += 1;
+            val-=1;
+        }
+        cout << '\n';
+        rows += 1;
+    }
+}
+
+// output
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+// 5 4 3 2 1
