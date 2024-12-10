28810b91d274178bdf24abb4f311576ed6ff2bfd
sujalcharati <sujalc024@gmail.com>
2024-12-10 09:13:34 +0530

    pattern 11

---
+++ pattern/p11.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number of rows";
+    cin >> n;
+    int rows = 1;
+     char start ='A';
+
+    while (rows <= n)
+    {
+        int col = 1;
+        while (col <= rows)
+        {   
+            cout << start << " ";
+            col += 1;
+            start+=1;
+        }
+        cout << '\n';
+        rows += 1;
+    }
+}
+
+// output
+// A B C D E
+// F G H I J
+// K L M N O 
+// P Q R S T 
+// U V W X Y
