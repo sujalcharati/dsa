c776386241a58d73b8187aa53b827de0ae7b47b9
sujalcharati <sujalc024@gmail.com>
2024-12-11 12:03:36 +0530

    pattern 15

---
+++ pattern/p15.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number of rows";
+    cin >> n;
+    int rows = 1;
+    while (rows <= n)
+    {
+        int space = n-rows;
+        while(space){
+            cout << " ";
+            space-=1;
+        }
+        int col = 1;
+        while (col <= rows)
+        {   
+            cout << "*" << " ";
+            col += 1;
+        }
+        cout << '\n';
+        rows += 1;
+    }
+}
+
+// output
+//     * 
+//    * * 
+//   * * * 
+//  * * * * 
+// * * * * * 
