b443e76c2d871d26b259e25980462a93bf5f1cc9
sujalcharati <sujalc024@gmail.com>
2025-07-15 14:19:35 +0530

    brute force

--- array/zeroTolast.go
+++ array/zeroTolast.go
@@ -1,5 +1,5 @@
 package main
-
+import "fmt"
 // Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 // Note that you must do this in-place without making a copy of the array.
@@ -35,3 +35,38 @@ func moveZeroes(nums []int)  {
 
 
 // what i learned is this is two pointer like parallel pointer where one pointer is busy in finding the non zero element and other is busy in keeping track of that non zero
+
+
+
+//brute force 
+// Online Go compiler to run Golang program online
+// Print "Try programiz.pro" message
+
+func makeittozeros( nums []int ) [] int{
+    n:=len(nums)
+    temp := [] int{}
+    
+    for i := 0;i< n;i++{
+        
+    if nums[i] != 0 {
+        temp = append(temp, nums[i])
+       }
+    }
+    
+    zerocount := len(nums)-len(temp)
+    
+    for i :=0; i < zerocount ; i++{
+        temp = append(temp,0)
+    }
+    return temp
+    
+}
+func main() {
+    
+    nums := [] int {0,1,0,2,3,0}
+    val := makeittozeros(nums)
+    fmt.Println(val)
+    
+    
+  fmt.Println("Try programiz.pro")
+}
