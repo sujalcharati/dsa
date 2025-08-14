ce218ab7f3d737160c2ac3717c8f8a61afe8782d
sujalcharati <sujalc024@gmail.com>
2025-08-14 23:30:58 +0530

    valid parthensis issur

---
+++ stack/valid_parentheses.go
@@ -0,0 +1,31 @@
+package stack
+
+func isValid(s string) bool {
+
+    var s1 []byte
+
+    for i:= 0; i < len(s) ; i++ {
+        
+        if s[i] == '(' || s[i] == '{' || s[i] == '[' {
+
+            s1 = append(s1,s[i])
+        } else if s[i] == ')' || s[i] == '}' || s[i] == ']'{
+
+            if len(s1) == 0 {
+                return false
+            }
+
+            top := s1[ len(s1)-1]
+
+            if s[i]=='(' && top == ')' ||  s[i]=='{' && top == '}' ||  s[i]=='[' && top == ']' {
+                s1 = s1[:len(s1)]
+            } else {
+                return false
+            }
+
+    
+        }
+
+    } 
+    return true    
+}
