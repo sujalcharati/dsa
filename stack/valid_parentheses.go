2f0f2c2461657044c62f732ad7e6be9f60dfd3e2
sujalcharati <sujalc024@gmail.com>
2025-08-15 18:56:56 +0530

    test case passed

--- stack/valid_parentheses.go
+++ stack/valid_parentheses.go
@@ -1,31 +1,34 @@
-package stack
-
 func isValid(s string) bool {
 
     var s1 []byte
 
+    if len(s) == 1 || len(s) == 0{
+        return false
+    }
     for i:= 0; i < len(s) ; i++ {
         
         if s[i] == '(' || s[i] == '{' || s[i] == '[' {
 
             s1 = append(s1,s[i])
-        } else if s[i] == ')' || s[i] == '}' || s[i] == ']'{
 
-            if len(s1) == 0 {
+        } 
+         if s[i] == ')' || s[i] == '}' || s[i] == ']'{
+
+            if len(s1) == 0  {
                 return false
             }
 
             top := s1[ len(s1)-1]
 
-            if s[i]=='(' && top == ')' ||  s[i]=='{' && top == '}' ||  s[i]=='[' && top == ']' {
-                s1 = s1[:len(s1)]
+            if s[i]==')' && top == '(' ||  s[i]=='}' && top == '{' ||  s[i]==']' && top == '[' {
+                s1 = s1[:len(s1)-1]
             } else {
                 return false
             }
 
-    
-        }
+         } 
+    }
 
-    } 
-    return true    
+
+    return len(s1) == 0    
 }
