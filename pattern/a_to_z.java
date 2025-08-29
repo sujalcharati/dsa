d8b1321745d12a4016259fe5a914019d30d500c9
sujalcharati <sujalc024@gmail.com>
2025-08-29 20:00:15 +0530

    pattern problems

---
+++ pattern/a_to_z.java
@@ -0,0 +1,226 @@
+ // link to question : https://takeuforward.org/strivers-a2z-dsa-course/must-do-pattern-problems-before-starting-dsa/
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+//         for(int i =0;i<5;i++){
+//             for(int j=0;j<5;j++){
+
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+//         for(int i =0;i<5;i++){
+//             for(int j=0;j<=i;j++){
+
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+//         for(int i =1;i<=5;i++){
+//             for(int j=1;j<=i;j++){
+
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+//         for(int i =1;i<=5;i++){
+//             for(int j=1;j<=i;j++){
+
+//                 System.out.print(i);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+//         for(int i =5;i>=1;i--){
+//             for(int j=1;j<=i;j++){
+
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+//         for(int i =5;i>=1;i--){
+//             for(int j=1;j<=i;j++){
+
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+  
+//         for (int i =0; i<5;i++){
+
+//             for (int j=0; j< 5-i-1;j++){
+//                 System.out.print("  ");
+//             }
+//             for (int k=0; k< 2*i+1;k++){
+//                 System.out.print(" * ");
+//             }
+//             for( int l=0;l< 5-i-1;l++){
+//                 System.out.print("");
+
+//             }
+//           System.out.println();
+//         }
+//     }
+// }
+
+
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+  
+//         for (int i =5; i>0;i--){
+
+//             for (int j=0; j< 5-i;j++){
+//                 System.out.print("  ");
+//             }
+//             for (int k=0; k< 2*i-1;k++){
+//                 System.out.print(" * ");
+//             }
+//             for( int l=0;l< 5-i;l++){
+//                 System.out.print(" ");
+
+//             }
+//           System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+  
+
+//             for(int j=0;j< 5;j++){
+
+//                 for( int k=0;k< 5-j-1;k++){
+//                     System.out.print(" ");
+//                 }
+//                 for( int k=0;k< 2*j+1;k++){
+//                     System.out.print("*");
+//                 }
+//                 for( int k=0;k<= 5-j-1;k++){
+//                     System.out.print(" ");
+//                 }
+//                 System.out.println();
+//             }
+
+//             for(int j=5;j>0;j--){
+
+//                 for( int k=0;k< 5-j;k++){
+//                     System.out.print(" ");
+//                 }
+//                 for(int k=0;k<2*j-1;k++){
+//                     System.out.print("*");
+//                 }
+//                 for(int k=0;k< 5-j;k++){
+//                     System.out.print(" ");
+//                 }
+//               System.out.println();
+//             }
+
+//           System.out.println();
+        
+//     }
+// }
+
+
+
+
+
+// package pattern;
+
+// public class a_to_z {
+    
+//     public static void main(String[] args) {
+  
+
+//             for(int j=0;j< 5;j++){
+
+//                 for( int k=0;k< j+1;k++){
+//                     System.out.print("*");
+//                 }
+        
+//                 System.out.println();
+//             }
+
+//             for(int j=5;j>0;j--){
+
+//                 for( int k=0;k< j-1;k++){
+//                     System.out.print("*");
+//                 }
+             
+//               System.out.println();
+//             }
+        
+//     }
+// }
+
+
