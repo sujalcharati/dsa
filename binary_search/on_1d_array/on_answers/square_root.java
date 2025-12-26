1fa0b0a81443846fad16c0bb605b3f8b6a80f2bd
sujalcharati <sujalc024@gmail.com>
2025-12-26 20:56:31 +0530

    sqrt

---
+++ binary_search/on_1d_array/on_answers/square_root.java
@@ -0,0 +1,75 @@
+// Brute force method :
+
+// package binary_search.on_1d_array.on_answers;
+
+// import java.util.*;
+
+// public class square_root {
+
+//     public static void main(String[] args){
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print(" Enter a number:");
+//         int val = sc.nextInt();
+
+//         for( int i=0; i<= val; i++){
+//             if( i*i == val) {
+//                 System.out.println(i);
+//               break;
+//             }
+//             if ( i*i > val) {
+//                 System.out.println( i-1);
+//                 break;
+//             }
+
+//         }
+
+//     }
+    
+// }
+
+
+
+// better approach 
+
+package binary_search.on_1d_array.on_answers;
+
+import java.util.*;
+
+import binary_search.on_1d_array.lowerBound;
+
+public class square_root {
+
+    public static int findSQRT( int val){
+
+        int left = 0;
+        int right = val;
+        int ans =0;
+
+        while( left < right){
+
+            int mid = left+ ( right-left)/2;
+            int anss = mid*mid;
+
+            if( anss == val ) return mid;
+            else if( anss > val) right = mid-1;
+            else {
+                ans =mid;
+                left = mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" Enter a number:");
+        int val = sc.nextInt();
+
+        int ans = findSQRT(val);
+        System.out.println(ans);
+
+    }
+    
+}
