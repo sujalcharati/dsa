ac8920d56b765faf6d3de57b68085388aa51a512
sujalcharati <sujalc024@gmail.com>
2025-12-30 23:18:52 +0530

    m boquets

--- binary_search/on_answers/mBoquets.java
+++ binary_search/on_answers/mBoquets.java
@@ -2,6 +2,56 @@
 
 // package binary_search.on_answers;
 
+// public class mBoquets {
+
+//     public static int findNoofBoquets(int[] arr, int k, int m) {
+
+//         int max = Integer.MIN_VALUE;
+//         int min = Integer.MAX_VALUE;
+
+//         for (int i = 0; i < arr.length; i++) {
+//             max = Math.max(max, arr[i]);
+//             min = Math.min(min, arr[i]);
+//         }
+
+//         for (int i = min; i <= max; i++) {
+//             int count = 0;
+//             int noofb = 0;
+//             for (int j = 0; j < arr.length; j++) {
+//                 if (arr[j] <= i) {
+//                     count++;
+
+//                 } else {
+//                     noofb += (count / k);
+//                     count = 0;
+//                 }
+//             }
+//             noofb += (count / k);
+
+//             if (noofb >= m) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String[] args) {
+
+//         int[] arr = { 7, 7, 7, 7, 13, 11, 12, 7 };
+//         int k = 3;
+//         int m = 2;
+
+//         int val = findNoofBoquets(arr, k, m);
+//         System.out.println(val);
+
+//     }
+// }
+
+
+
+// binary search optimal solution:
+
+
 public class mBoquets {
 
     public static int findNoofBoquets(int[] arr, int k, int m) {
@@ -14,25 +64,33 @@ public class mBoquets {
             min = Math.min(min, arr[i]);
         }
 
-        for (int i = min; i <= max; i++) {
-            int count = 0;
-            int noofb = 0;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] <= i) {
+        while( min <= max){
+            int mid = min +( max-min)/2;
+            
+            int count=0;
+            int noofb =0;
+             for( int i=0; i< arr.length; i++){
+    
+
+                if( arr[i] <= mid){
                     count++;
-
-                } else {
-                    noofb += (count / k);
-                    count = 0;
+                } else{
+                    noofb+= (count/k);
+                    count=0;
                 }
-            }
-            noofb += (count / k);
 
-            if (noofb >= m) {
-                return i;
+            }
+            noofb += (count/k);
+            if( noofb >= m){
+                 max = mid-1;
+            }else{
+                min = mid+1;
             }
         }
-        return -1;
+
+        return min;
+
+        
     }
 
     public static void main(String[] args) {
