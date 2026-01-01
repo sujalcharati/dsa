d2118d3f9c76694026fb7068eab95493b7104363
sujalcharati <sujalc024@gmail.com>
2026-01-01 10:51:07 +0530

    smallest divisor

---
+++ binary_search/on_answers/smallestDivisor.java
@@ -0,0 +1,40 @@
+public class smallestDivisor {
+
+
+    public static int findSmallestDivisor( int[] nums, int threshold){
+
+        int max = Integer.MIN_VALUE;
+        int divisor = Integer.MAX_VALUE;
+
+        for( int i=0; i< nums.length;i++){
+            max = Math.max(max,nums[i]);
+        }
+
+        for( int d=1;d <= max; d++){
+            int sum =0;
+
+            for( int i=0; i< nums.length;i++){
+                sum+= (nums[i]+d-1)/d;
+
+            }
+
+            if( sum <= threshold){
+                divisor = d;
+                break;
+            }
+        }
+
+        return divisor;
+    }
+
+
+    public static void main(String[] args) {
+        
+        int[] nums = {1,2,5,9};
+        int threshold = 6;
+
+        int val = findSmallestDivisor(nums,threshold);
+        System.out.println(val);
+    }
+    
+}
