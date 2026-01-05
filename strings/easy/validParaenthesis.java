d6bd322873d8ae7c575c24af7663624cdf0cecbd
sujalcharati <sujalc024@gmail.com>
2026-01-05 19:49:45 +0530

    remove parenthesis

--- strings/easy/validParaenthesis.java
+++ strings/easy/validParaenthesis.java
@@ -3,7 +3,28 @@ package strings.easy;
 public class validParaenthesis {
 
     public static void main(String[] args) {
+
+
+        String str = "()(()())(())";
+        int count=0;
+        String ans = "";
         
+        for(int i=0; i< str.length(); i++){
+
+            if( str.charAt(i) == ')'){
+                count--;
+        } 
+        if( count !=0){
+             ans+= str.charAt(i);
+        }
+        if( str.charAt(i) == '('){
+            count++;
+        }
+
+        }
+        System.out.print(ans);
+        
+
         
     }
     
