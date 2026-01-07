0ef4c8e967e686d9c4b8c971247f1e2c5b18e111
sujalcharati <sujalc024@gmail.com>
2026-01-08 00:33:11 +0530

    toCharArray()

---
+++ strings/docs/stringToChar.java
@@ -0,0 +1,24 @@
+package strings.docs;
+
+import java.util.Arrays;
+
+public class stringToChar {
+
+    public static void main(String[] args) {
+        String s = "sujal";
+        char[] ch = s.toCharArray(); // s u j a l
+        for( char item : ch){
+            System.out.print( item+" ");
+        }
+
+
+        String[] ans = { "sujal", "suchi","sua","suzal"}; //sua suchi sujal suzal
+        Arrays.sort(ans);
+
+          for( String str : ans){
+
+              System.out.println(str);
+          }
+    }
+    
+}
