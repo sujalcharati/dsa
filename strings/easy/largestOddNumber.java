6cae9f30ec31292abd64f2ce52971e927fa220f9
sujalcharati <sujalc024@gmail.com>
2026-01-10 22:53:23 +0530

    largest odd number

--- strings/easy/largestOddNumber.java
+++ strings/easy/largestOddNumber.java
@@ -3,8 +3,21 @@ package strings.easy;
 public class largestOddNumber {
 
     public static void main(String[] args) {
-       String s = "0214638";
-       String val = 
+       String s = "02468";
+
+       StringBuilder sb = new StringBuilder(s);
+
+       for( int i= s.length()-1; i>=0; i--){
+
+        int digit = s.charAt(i)-'0';
+
+        if( digit %2 !=0){
+            sb.setLength(i+1);
+            System.out.println(sb.toString());
+            break;
+        }
+       }
+          System.out.println(" ");       
     }
     
 }
