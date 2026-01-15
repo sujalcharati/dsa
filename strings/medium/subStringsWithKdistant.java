0453883623c4f9ef13c5f3c9247434ac6a444a26
sujalcharati <sujalc024@gmail.com>
2026-01-15 23:56:47 +0530

    substrings

---
+++ strings/medium/subStringsWithKdistant.java
@@ -0,0 +1,24 @@
+// link to this problem : https://www.geeksforgeeks.org/problems/count-number-of-substrings4528/1
+
+package strings.medium;
+
+public class subStringsWithKdistant {
+
+
+    static int findDistinctSubstrings( String s, int k){
+
+        
+    }
+
+    public static void main(String[] args) {
+        
+        String s = "abc";
+        int k =2 ;
+
+        int val = findDistinctSubstrings(s,k);
+        System.out.println(val);
+    }
+
+
+    
+}
