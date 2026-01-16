64ded8293c78e822bd7e00f66c7599d2e634e607
sujalcharati <sujalc024@gmail.com>
2026-01-16 21:06:52 +0530

    linked list

---
+++ linkedlist/linkedlist.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+public class linkedlist {
+
+    public class Node {
+        
+        int value;
+        Node next;
+
+        Node( int value){
+            this.next = null;
+            this.value = value;
+        }
+        
+    }
+
+    
+}
