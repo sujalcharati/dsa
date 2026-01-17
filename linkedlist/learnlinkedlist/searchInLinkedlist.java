161882bdbfacf891f2eadfd40da80ccacc753f0d
sujalcharati <sujalc024@gmail.com>
2026-01-17 23:10:45 +0530

    search in linkedlist

---
+++ linkedlist/learnlinkedlist/searchInLinkedlist.java
@@ -0,0 +1,41 @@
+/* Defination of ListNoode
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int value) {
+        this.val = value;
+        this.next = null;
+    }
+}
+*/
+
+
+// class Solution {
+//     public boolean searchKey(ListNode head, int key) {
+//         // Your code goes here
+        
+//         ListNode temp = head;
+
+        //   if( temp == null) return false;
+//         while( temp.next != null){
+
+//             if( temp.val == key){
+//                 return true;
+//             }
+//             temp = temp.next;
+//         }
+
+//         if( temp.val == key) return true;
+//         return false;
+//     }
+// }
+
+
+
+
+// package linkedlist.learnlinkedlist;
+
+// public class searchInLinkedlist {
+    
+// }
