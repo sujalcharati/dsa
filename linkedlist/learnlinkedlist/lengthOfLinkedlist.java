02e2485039dcb99f8eb0c84985be238f0de8c71e
sujalcharati <sujalc024@gmail.com>
2026-01-17 23:10:12 +0530

    length of linkedlist

---
+++ linkedlist/learnlinkedlist/lengthOfLinkedlist.java
@@ -0,0 +1,13 @@
+
+// class Solution {
+//     public int getLength(ListNode head) {
+//         // Your code goes here
+//         int len =0;
+//         ListNode temp = head;
+//         while ( temp.next != null){
+//             temp = temp.next;
+//             len++;
+//         }
+//         return len+1;
+//     }
+// }
