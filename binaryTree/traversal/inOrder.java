054b878187d7c4ba1d343de743c7438b93f8d789
sujalcharati <sujalc024@gmail.com>
2026-04-12 12:01:15 +0530

    Add in-order traversal

---
+++ binaryTree/traversal/inOrder.java
@@ -0,0 +1,33 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+
+//         List<Integer> ans = new ArrayList<>();
+//         solve( root, ans);
+//         return ans;
+        
+//     }
+
+//     public void solve( TreeNode root, List<Integer> ans){
+
+//         if( root == null ) return;
+
+//         solve( root.left, ans);
+//         ans.add(root.val);
+//         solve( root.right, ans);
+//     }
+// }
