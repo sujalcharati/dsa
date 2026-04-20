f97ed7b4e7cc37439880abfe6ae677735cd58dde
sujalcharati <sujalc024@gmail.com>
2026-04-20 11:31:13 +0530

    Add diameter of binary tree

---
+++ binaryTree/medium/diameter.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+// class Solution {
+//     int maxi = 0;
+    
+//     public int diameterOfBinaryTree(TreeNode root) {
+
+//          height( root);
+//          return maxi;
+       
+//     }
+
+//     private int height(TreeNode root){
+
+//      if( root == null ) return 0;
+
+//         int lf = height( root.left);
+//         int rf = height( root.right);
+
+//         maxi = Math.max( maxi, lf+rf);
+        
+//         return 1+ Math.max(lf, rf);
+
+//     }
+
+        
+// }
